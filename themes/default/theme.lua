17c57a7356c4fa3c0c85522fa6d68a4aa17051c9
bayram-kazik <48856944+bayram-kazik@users.noreply.github.com>
2021-08-29 17:55:28 +0300

    Change some colors in default theme

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -14,8 +14,8 @@ local theme = {}
 theme.font          = "Fira Code Retina 9"
 
 theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
-theme.bg_urgent     = "#ff0000"
+theme.bg_focus      = "#660094"
+theme.bg_urgent     = "#d16aff"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
 
@@ -27,8 +27,8 @@ theme.fg_minimize   = "#ffffff"
 theme.useless_gap   = dpi(2)
 theme.border_width  = dpi(2)
 theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
-theme.border_marked = "#91231c"
+theme.border_focus  = "#660094"
+theme.border_marked = "#d16aff"
 
 -- There are other variable sets
 -- overriding the default one when
