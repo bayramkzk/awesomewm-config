a46aeeb8b901b76dbe6679f1f6f50919bee7ce9c
bayram-kazik <48856944+bayram-kazik@users.noreply.github.com>
2021-08-29 17:52:40 +0300

    Replace wallpaper of default theme

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -7,7 +7,7 @@ local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
-local themes_path = gfs.get_themes_dir()
+local themes_path = os.getenv("HOME").."/.config/awesome/themes/"
 
 local theme = {}
 
@@ -97,7 +97,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = themes_path.."default/background.png"
+theme.wallpaper = themes_path.."default/wallpaper.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
