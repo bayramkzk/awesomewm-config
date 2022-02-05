47eb3a6e4f49f95ece40ab2728b94611fca01f23
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-02-05 23:31:31 +0300

    Refactor mouse configuration, update autorun.sh

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 function run {
   if ! pgrep -f $1 ;
@@ -7,5 +7,20 @@ function run {
   fi
 }
 
-run ~/.config/awesome/scripts/mouse_config.sh
+# Mouse / Touchpad
+xinput set-prop 14 296 -1
+xinput set-prop 16 308 1
+xinput set-prop 16 285 1
 
+# Keyboard
+xset led
+setxkbmap -layout "us,de" -option "grp:alt_shift_toggle"
+
+# Wallpaper
+feh --bg-max ~/Pictures/Wallpapers/jupiter-close.jpg
+
+# Sound server
+run pulseaudio
+
+# Compositor
+run picom
