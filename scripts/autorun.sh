de066b2b3e934563eeb3e6d6f81ad251f594774f
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-02-06 03:25:05 +0300

    Update autorun.sh

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -7,16 +7,15 @@ function run {
   fi
 }
 
-# Mouse / Touchpad
+# Input
 xinput set-prop 14 296 -1
 xinput set-prop 16 308 1
 xinput set-prop 16 285 1
-
-# Keyboard
 xset led
 
-# Sound server
-run pulseaudio
+# Sound
+pulseaudio --start
 
-# Compositor
+# Programs
 run picom
+run flameshot
